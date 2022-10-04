f6a2f14a59026e922402982f2a893c53afafeea4
AltynbekChotonov <dtvkanalnews@gmail.com>
2022-10-04 11:50:31 +0300

    first commit

---
+++ lib/mocks/mock_all_destinations.dart
@@ -0,0 +1,10 @@
+import '../models/destination.dart';
+
+class MockAllDestinations {
+  static const data = [
+    Destination(city: 'Nassau', country: 'Баг,аралы', rating: '4.6'),
+    Destination(city: 'Mykonos', country: 'Греция', rating: '4.8'),
+    Destination(city: 'Rome', country: 'Италия', rating: '4.4'),
+    Destination(city: 'London', country: 'Англия', rating: '4.5'),
+  ];
+}
---
+++ lib/mocks/mock_top_destinations.dart
@@ -0,0 +1,9 @@
+import '../models/destination.dart';
+
+class MockTopDestinations {
+  static const data = [
+    Destination(city: 'Paris', country: 'Франция', rating: '4.8'),
+    Destination(city: 'Tahiti', country: 'Француз Полинезиясы', rating: '4.8'),
+    Destination(city: 'Mykonos', country: 'Греция', rating: '4.8'),
+  ];
+}
